2aefbb78c9ee98f287752d661cab28bea3fbf002
Jose Bellido <a_jose.martin.bellido@iespablopicasso.es>
2023-11-22 19:56:42 +0100

    Sobrecarga del método imprimeEnIntervalos y modificación de main

--- src/bucles/BucleAleatorios.java
+++ src/bucles/BucleAleatorios.java
@@ -13,7 +13,7 @@ public class BucleAleatorios {
     //Definición del programa para el usuario
     System.out.println("Programa de impresión de números enteros en intervalos.");
     
-    // Solicitamos los números enteros para el intervalo. Todo ello irá en una estructura try/catch.
+    // Solicitamos los números enteros para el intervalo y el número intermedio. Todo ello irá en una estructura try/catch.
     try {
       System.out.print("Introduzca un valor entero para el intervalo: ");
       int numero1 = Integer.parseInt(sc.nextLine());
@@ -21,8 +21,12 @@ public class BucleAleatorios {
       System.out.print("Introduzca segundo valor entero para el intervalo: ");
       int numero2 = Integer.parseInt(sc.nextLine());
       
+      System.out.print("Introduzca un tercer valor entero. "
+          + "Si se encuentra en el intervalo de los dos anteriores, se imprimirá por pantalla: ");
+      int numeroIntervalo = Integer.parseInt(sc.nextLine());
+      
       //Usamos el método que imprime los números dentro del intervalo
-      imprimeEnIntervalos(numero1,numero2);
+      imprimeEnIntervalos(numero1,numero2,numeroIntervalo);
       
     } catch (NumberFormatException e) {
       // Mensaje de error en caso de que no se introduzca uno de los valores en el formato deseado
@@ -46,7 +50,7 @@ public class BucleAleatorios {
     }
   }
   
-  // Método imprimeEnIntervalos
+  // Método imprimeEnIntervalos con dos valores
   public static void imprimeEnIntervalos(int numero1, int numero2) {
     
     // Definimos el límite inferior y superior en función de los distintos casos para trabajar sobre ellos sin tener que distinguir.
@@ -69,14 +73,40 @@ public class BucleAleatorios {
         limiteInferior += 7;
 
       }
-      
-      
-      
-      
-      
     }
+  }
+  
+  // Método imprimeEnIntervalos con tres valores
+  public static void imprimeEnIntervalos(int numero1, int numero2, int numeroIntermedio) {
     
+    // Definimos el límite inferior y superior en función de los distintos casos para trabajar sobre ellos sin tener que distinguir.
+    int limiteInferior;
+    int limiteSuperior;
     
+    // Nuestro programa sólo funciona mientras que ambos números sean distintos. Sino, no imprimimos nada.
+    if(numero1 != numero2) {
+      if (numero2 < numero1) {
+        limiteInferior = numero2;
+        limiteSuperior = numero1;
+      } else {
+        limiteInferior = numero1;
+        limiteSuperior = numero2;
+      }
+      
+      // Imprimimos mientras el límite inferior (variable que iremos aumentando) siga siendo menor o igual al límite superior.
+      while (limiteInferior <= limiteSuperior) {
+        System.out.println(limiteInferior);
+        
+        // Si el tercer número se encuentra en el intervalo, lo imprimimos respetando el orden. 
+        // Si es igual a uno de los límites, saldrá repetido.
+        if (numeroIntermedio >= limiteInferior && numeroIntermedio <= limiteInferior + 6) {
+          System.out.println(numeroIntermedio);
+        }
+        
+        limiteInferior += 7;
+
+      }
+    }
   }
   
 }
