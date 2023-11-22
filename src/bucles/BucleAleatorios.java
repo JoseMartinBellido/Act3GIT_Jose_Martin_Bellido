21df92b0f7a1ab74b0dd9fd3b3c9f2fff3e88543
Jose Bellido <a_jose.martin.bellido@iespablopicasso.es>
2023-11-22 10:03:40 +0100

    Creación del proyecto y clase BucleAleatorios

---
+++ src/bucles/BucleAleatorios.java
@@ -0,0 +1,20 @@
+package bucles;
+
+import java.util.Random;
+
+public class BucleAleatorios {
+
+  public static void imprimeAleatorios() {
+    
+    // Usamos la clase Random de Java de forma auxiliar para generar los números aleatorios
+    Random random = new Random();
+    
+    // Creamos un bucle que imprima 20 números del 1 al 10.
+    for (int i = 0; i < 20; i++) {
+      
+      int numAleatorio = random.nextInt(1,10);
+      System.out.println(numAleatorio);
+    }
+  }
+  
+}
